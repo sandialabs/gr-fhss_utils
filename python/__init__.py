6792e5bddd0f8181f09c7738cc1bc87075b34d9d
nobody <martinrp@gatech.edu>
2019-10-07 21:22:49 -0700

    update port to 3.8

--- python/__init__.py
+++ python/__init__.py
@@ -22,7 +22,6 @@
 This is the GNU Radio FHSS_UTILS module. Place your Python package
 description here (python/__init__.py).
 '''
-from __future__ import unicode_literals
 
 # import swig generated symbols into the fhss_utils namespace
 try:
